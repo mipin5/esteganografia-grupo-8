05b8ebdf803ef5396247387670f30d96880293a9
Isaac P <isaacpro2000@hotmail.com>
2021-04-14 18:34:28 -0500

    Update views.py

--- StegLock/website/views.py
+++ StegLock/website/views.py
@@ -14,17 +14,23 @@ def home():
 @views.route('/encrypt', methods=['GET', 'POST'])
 def encrypt():
     if request.method == 'POST':
-        medium = request.form.get('medium')
+        medium = request.files['medium']
         message = request.form.get('message')
         password = request.form.get('password')
-        medium_name = medium[:-4]
-        medium_type = medium[-3:]
         if len(message) < 1:
             flash('El mensaje es muy corto', category='error')
         else:
-            new_medium = Medium(name=medium_name, mtype=medium_type, password=password, user_id=current_user.id)
+            medium_name = secure_filename(medium.filename)
+            medium_type = medium.medium_type
+            medium_password = generate_password_hash(password, method='sha256')
+            if medium_type != 'png'
+                converted_img = Image.open(medium).convert('RGB')
+                converted_img.save(f'{medium_name}{.png}', 'png')
+            medium_file = converted_img.read()
+            new_medium = Medium(name=medium_name, mtype=medium_type, password=medium_password, user_id=current_user.id)
             db.session.add(new_medium)
             db.session.commit()
+            encoded_image = encodeStringInImage(message, medium_file, medium_password)
             flash('Medio encriptado!', category='success')
 
     return render_template("encrypt.html", user=current_user)
