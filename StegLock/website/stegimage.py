626c615d4a8076cfb723364e47451d1925830f16
jsabedrabbo <juanse.abedrabbo@gmail.com>
2021-04-19 11:06:45 -0500

    text file encryption/decryption

--- StegLock/website/stegimage.py
+++ StegLock/website/stegimage.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 # Transformar cadena a octetos
 def transformStringToBytes(data):
+    data = f'str={data}=str'
     dataBytes = []
 
     for i in data:
@@ -10,23 +11,16 @@ def transformStringToBytes(data):
     return dataBytes
 
 #Transformar pdf a octetos
-def transformPdfToBytes(file):
-    pass
+def transformFileToBytes(file):
+    with open(f'temp/{file}', 'r') as f:
+        fileData = f.read()
+    fileData = f'txt={fileData}=txt'
+    dataBytes = []
 
-#Transformar octetos a cadena
-def transformBytesToString(bytes):
-    data = ''
-    
-    for octet in bytes:
-        data += chr(int(octet[:8], 2))
-        if (octet[-1] == 0):
-            break
+    for i in fileData:
+        dataBytes.append(format(ord(i), '08b'))
 
-    return data
-
-#Transformar octetos a pdf
-def transformBytesToPdf(bytes):
-    pass
+    return dataBytes
 
 #Codificar cadena en imagen
 def encodeStringInImage(data, image, password='null'):
@@ -72,13 +66,53 @@ def encodeStringInImage(data, image, password='null'):
     
     return image
 
+#Codificar archivo en imagen
+def encodeFileInImage(file, image, password='null'):
+    password = f'pwd={password}=pwd'
 
-#Codificar pdf en imagen
-def encodePdfInImage(file, image):
-    pass
+    dataBytes = transformFileToBytes(file)
+    passwordBytes = transformStringToBytes(password)
+    fullData = passwordBytes + dataBytes
+    dataLength = len(fullData)
+    image = image.convert('RGB')
 
-#Decodificar cadena de imagen
-def decodeStringFromImage(image, password='null'):
+    # Coordenadas en imagen
+    i = 0
+
+    breakLoop = False
+    for x in range(image.width):
+        for y in range(image.height):
+            (r, g, b) = image.getpixel((x, y))
+            red = format(r, '08b')
+            green = format(g, '08b')
+            blue = format(b, '08b')
+            red = str(red)
+            green = str(green)
+            blue = str(blue)
+            red = red[:5]
+            green = green[:5]
+            blue = blue[:5]
+            red += fullData[i][:3]
+            green += fullData[i][3:6]
+            blue += fullData[i][6:]
+            if(i != dataLength - 1):
+                blue += '1'
+            else:
+                blue += '0'
+                breakLoop = True
+            newRed = int(red, 2)
+            newGreen = int(green, 2)
+            newBlue = int(blue, 2)
+            image.putpixel((x, y), (newRed, newGreen, newBlue))
+            i += 1
+            if breakLoop: break
+        if breakLoop: break
+    
+    return image
+
+
+#Decodificar imagen
+def decodeImage(image, password='null'):
     message = ''
     data = ''
 
@@ -105,11 +139,21 @@ def decodeStringFromImage(image, password='null'):
     
     if (password == imagePassword):
         data = message[endIndex + 4:]
+        if 'str=' in data and '=str' in data:
+            startIndex = data.find('str=') + 4
+            endIndex = data.find('=str')
+            data = data[startIndex:endIndex]
+        elif 'txt=' in data and '=txt' in data:
+            startIndex = data.find('txt=') + 4
+            endIndex = data.find('=txt')
+            data = data[startIndex:endIndex]
+            filepath = 'temp/decodedmessage.txt' 
+            with open(f'{filepath}', 'w') as f:
+                f.write(data)
+            data = filepath
+        else:
+            data = '0'
     else:
         data = '0'
 
     return data
-
-#Decodificar pdf de imagen
-def decodePdfFromImage(image):
-    pass
--- StegLock/website/views.py
+++ StegLock/website/views.py
@@ -1,14 +1,29 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, send_file
 from flask_login import login_required, current_user
 from .models import Medium
 from . import db
 from PIL import Image
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
-import json
+import json, os
+from io import BytesIO
+from .stegimage import encodeStringInImage, encodeFileInImage, decodeImage
 
 views = Blueprint('views', __name__)
 
+#Crea la carpeta temp dentro del directorio principal del proyecto si no existe
+def createFolder():
+    created_upload_path = 'temp/'
+    try:
+        if not os.path.exists(created_upload_path):
+            os.makedirs(created_upload_path)
+    except OSError:
+        print ('Error creando directorio' + created_upload_path)
+
+def deleteTempFiles():
+    directory = 'temp/'
+    for f in os.listdir(directory):
+        os.remove(os.path.join(directory, f))
 
 @views.route('/', methods=['GET'])
 def home():
@@ -16,24 +31,96 @@ def home():
 
 @views.route('/encrypt', methods=['GET', 'POST'])
 def encrypt():
-    if request.method == 'POST':
-        medium = request.files['medium']
-        message = request.form.get('message')
-        password = request.form.get('password')
-        if not medium:
-            flash('No se encontró ningún medio', category='error')
-        elif len(message) < 1:
-            flash('El mensaje es muy corto', category='error')
-        else:
-            medium_name = secure_filename(medium.filename)
-            medium_type = medium.mimetype
-            medium_password = generate_password_hash(password, method='sha256')
-            new_medium = Medium(name=medium_name, mtype=medium_type, password=medium_password, user_id=current_user.id)
-            db.session.add(new_medium)
-            db.session.commit()
-            flash('Medio encriptado!', category='success')
+    try:  
+        if request.method == 'POST':
+            deleteTempFiles()
+            medium = request.files['medium']
+            message = request.form.get('message')
+            textfile = request.files['textfile']
+            password = request.form.get('password')
+            if not medium:
+                flash('No se encontró ningún medio', category='error')
+            elif not textfile and len(message) < 1:
+                flash('No se ha encontrado elemento a encriptar', category='error')
+            elif textfile and len(message) > 0:
+                flash('No se puede ingresar más de un elemento a encriptar', category='error')
+            else:
+                medium_name = secure_filename(medium.filename)
+                medium_type = medium.mimetype
+                medium_password = generate_password_hash(password, method='sha256')
 
-    return render_template("encrypt.html", user=current_user)
+                #Convierte imagen a png
+                ima = Image.open(medium)
+                f = BytesIO()
+                ima.save(f, format='PNG')
+                f.seek(0)
+                medium_file = Image.open(f)
+
+                #Registra en base de datos
+                new_medium = Medium(name=medium_name, mtype=medium_type, password=medium_password, user_id=current_user.id)
+                db.session.add(new_medium)
+                db.session.commit()
+
+                #Codifica de acuerdo a si se ingresó una cadena o un archivo de texto
+                createFolder()
+                if len(message) > 0:
+                    encoded_image = encodeStringInImage(message, medium_file, password)
+                else:
+                    textfile.save(f'temp/textfile.txt')
+                    encoded_image = encodeFileInImage('textfile.txt', medium_file, password)
+
+                #Guarda la imagen resultado en archivo temporal
+                encoded_image.seek(0)
+                encoded_image.save(f'temp/encoded.png')
+
+                #Crea la respuesta del servidor como descarga de la imagen y elimina la imagen temporal
+                upload_path = f'../temp/encoded.png' #send_file() lee desde la ubicación del archivo views.py
+                flash('Medio encriptado!', category='success')
+                return send_file(upload_path, as_attachment=True)
+                #os.remove(f'../temp/encoded.png')
+
+        return render_template("encrypt.html", user=current_user)
+    
+    except:
+        flash("Error del sistema", category='error')
+        return render_template("encrypt.html", user=current_user)
+
+@views.route('/decrypt', methods=['GET', 'POST'])
+def decrypt():
+    try:
+        if request.method == 'POST':
+            deleteTempFiles()
+            medium = request.files['medium']
+            password = request.form.get('password')
+            if not medium:
+                flash('No se encontró ningún medio', category='error')
+            else:
+                #Convierte imagen a png
+                ima = Image.open(medium)
+                f = BytesIO()
+                ima.save(f, format='PNG')
+                f.seek(0)
+                medium_file = Image.open(f)
+
+                decoded_result = decodeImage(medium_file, password)
+                download_path = f'../temp/decodedmessage.txt'
+
+                if decoded_result == 'temp/decodedmessage.txt':
+                    flash('Medio desencriptado!', category='success')
+                    return send_file(download_path, as_attachment=True)
+                elif decoded_result == '0':
+                    flash("Contraseña incorrecta!", category='error')
+                    return render_template("decrypt.html", user=current_user)
+                else:
+                    flash('Medio desencriptado!', category='success')
+                    return render_template("decrypt.html", user=current_user, decoded_message=decoded_result)
+        
+        return render_template("decrypt.html", user=current_user)
+
+    except:
+        flash("Error del sistema", category='error')
+        return render_template("decrypt.html", user=current_user)
+
 
 @views.route('/activity', methods=['GET'])
 def activity():
